a93c0ce880acab23bba9176711245c61ec44f817
Jalen Moy <jalen.r.moy.mil@swf.army.mil>
2025-08-03 14:32:38 -0500

    the messege

--- hello_kevin.sh
+++ hello_kevin.sh
@@ -1,6 +1,11 @@
+fewafew 
+
+
+
 #! /bin/sh
 
 echo "Hello Kevin"
 
 echo"BYE Kevin" fefeafewaffffffuibfiewbafiewbaiucfvewyuavfewuyabfckdsankclbauyebaiufewbacdbshjkbdsmnbcjdabfe
 
+I need to learn now to do this who feiownfiuewbanf iuewbaiufbn ijlfnewkjn fefewaf eoinfiue wnoifen ifewq
